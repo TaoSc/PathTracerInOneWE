757aef2ae4a311b47bc6247681ada91d7a94ef32
Tao Schreiner <taoschreiner@outlook.com>
2020-03-24 22:03:36 +0100

    Added dielectrics

--- material.cpp
+++ material.cpp
@@ -5,3 +5,16 @@
 vec3 reflect(const vec3& incident, const vec3& normal) {
     return incident - 2 * dot(incident, normal) * normal; // we compute a ray perpendicular to the incident one
 }
+
+vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+    auto cos_theta = dot(-uv, n);
+    vec3 r_out_parallel = etai_over_etat * (uv + cos_theta * n);
+    vec3 r_out_perp = -sqrt(1.0 - r_out_parallel.length_squared()) * n;
+    return r_out_parallel + r_out_perp;
+}
+
+double schlick(double cosine, double ref_idx) {
+    auto r0 = (1 - ref_idx) / (1 + ref_idx);
+    r0 = r0 * r0;
+    return r0 + (1 - r0) * pow((1 - cosine), 5);
+}
--- material.h
+++ material.h
@@ -1,24 +1,27 @@
 #pragma once
 
+#include <algorithm>
+
 #include "ray.h"
 #include "hittable.h"
 #include "random.h"
 
 vec3 reflect(const vec3& v, const vec3& n);
+vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat);
+double schlick(double cosine, double ref_idx);
 
 class material {
 public:
     virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const = 0;
 };
 
-
 class lambertian : public material {
 public:
     lambertian(const vec3& a) : albedo(a) {}
 
     virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
-        vec3 target = rec.point + rec.normal + random_in_unit_sphere();
-        scattered = ray(rec.point, target - rec.point);
+        vec3 scatter_direction = rec.normal + random_unit_vector();
+        scattered = ray(rec.point, scatter_direction);
         attenuation = albedo;
         return true;
     }
@@ -26,12 +29,9 @@ public:
     vec3 albedo;
 };
 
-
 class metal : public material {
 public:
-    metal(const vec3& a, const float& f) : albedo(a) {
-        fuzz = (f > 1 || f < 0) ? 1 : f;
-    }
+    metal(const vec3& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
     virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
         vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
@@ -41,5 +41,36 @@ public:
     }
 
     vec3 albedo;
-    float fuzz;
+    double fuzz;
+};
+
+class dielectric : public material {
+public:
+    dielectric(double ri) : ref_idx(ri) {}
+
+    virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+        attenuation = vec3(1.0, 1.0, 1.0);
+        double etai_over_etat = (rec.front_face) ? (1.0 / ref_idx) : (ref_idx);
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+        double cos_theta = std::min(dot(-unit_direction, rec.normal), 1.0);
+        double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+        if (etai_over_etat * sin_theta > 1.0) {
+            vec3 reflected = reflect(unit_direction, rec.normal);
+            scattered = ray(rec.point, reflected);
+            return true;
+        }
+        double reflect_prob = schlick(cos_theta, etai_over_etat);
+        if (random_double() < reflect_prob)
+        {
+            vec3 reflected = reflect(unit_direction, rec.normal);
+            scattered = ray(rec.point, reflected);
+            return true;
+        }
+        vec3 refracted = refract(unit_direction, rec.normal, etai_over_etat);
+        scattered = ray(rec.point, refracted);
+        return true;
+    }
+
+    double ref_idx;
 };
