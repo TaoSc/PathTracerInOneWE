bb42a8877b611ffaf31a0f7c2dff41c29bb39f4c
Tao Schreiner <taoschreiner@outlook.com>
2020-03-28 13:23:11 +0100

    vec3 class improvements

--- utilities/vec3.h
+++ utilities/vec3.h
@@ -2,10 +2,12 @@
 
 #include <iostream>
 #include <cmath>
+#include <algorithm>
 
 class vec3 {
 public:
     vec3() : e{ 0,0,0 } {}
+    vec3(double v) : e{ v, v, v } {}
     vec3(double e0, double e1, double e2) : e{ e0, e1, e2 } {}
 
     double x() const { return e[0]; }
@@ -23,14 +25,14 @@ public:
         return *this;
     }
 
-    vec3& operator*=(const double t) {
+    vec3& operator*=(const double& t) {
         e[0] *= t;
         e[1] *= t;
         e[2] *= t;
         return *this;
     }
 
-    vec3& operator/=(const double t) {
+    vec3& operator/=(const double& t) {
         return *this *= 1 / t;
     }
 
@@ -42,10 +44,10 @@ public:
         return e[0] * e[0] + e[1] * e[1] + e[2] * e[2]; // dot squared
     }
 
-    void output(unsigned char* data, int* index) {
-        data[(*index)++] = static_cast<unsigned char>(255.99f * sqrt(e[0]));
-        data[(*index)++] = static_cast<unsigned char>(255.99f * sqrt(e[1]));
-        data[(*index)++] = static_cast<unsigned char>(255.99f * sqrt(e[2]));
+    template <typename T> void output(T* data, size_t* index) {
+        data[(*index)++] = static_cast<T>(256 * std::clamp(sqrt(e[0]), 0., 0.999));
+        data[(*index)++] = static_cast<T>(256 * std::clamp(sqrt(e[1]), 0., 0.999));
+        data[(*index)++] = static_cast<T>(256 * std::clamp(sqrt(e[2]), 0., 0.999));
     }
 
     double e[3];
@@ -67,15 +69,15 @@ inline vec3 operator*(const vec3& u, const vec3& v) {
     return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
-inline vec3 operator*(double t, const vec3& v) {
+inline vec3 operator*(const double& t, const vec3& v) {
     return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
 }
 
-inline vec3 operator*(const vec3& v, double t) {
+inline vec3 operator*(const vec3& v, const double& t) {
     return t * v;
 }
 
-inline vec3 operator/(const vec3& v, double t) {
+inline vec3 operator/(const vec3& v, const double& t) {
     return (1 / t) * v;
 }
 
